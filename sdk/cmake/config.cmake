6e807aa108c9630687f9be3200c57c45bfc64a7c
Timo Kreuzer <timo.kreuzer@reactos.org>
2019-08-18 20:38:45 +0200

    [SPEC2DEF] Implement generating export patch information
    Now spec2def uses the version annotations in spec files to generate a table, which will be used to dynamically patch the export table based on the current process' appcompat settings, when a dll is initialized, allowing to export the appropriate functions for each version.
    
    This feature is still disabled in config.cmake with the variable DISABLE_EXPORT_VERSIONING.
    
    [NTDLL] Add mechanism to patch the export table at runtime
    
    The loader checks for a ros-compat-descriptor, and if one is found, parses it's entries. Each entry corresponds to an export and provides a bitmask that specifies on what Windows version the export exists. If the bit for the process' appcompat version (default is still Windows 2003) is set, the export will be kept, otherwise the export will be moved into a second (private) export table. If a module has a ros-compat-descritor, it is still allowed to import these hidden exports.
    
    [ROSCOMPAT_APITEST] Add initial test for export rewriting
    
    [COMCTL32] Include roscompat descriptor
    
    [WINHTTP] Include roscompat descriptor,
    so that winhttp can use nt6+ apis
    
    [REACTOS] Make sure xxx_vista is linked to before xxx
    
    This is required, because with dynamically patched export tables we will export all vista functions from the main dll, and you can link to them at compile time, but they will not be resolved at runtime, unless the importer has a roscompat-descriptor. Linking the vista version first will resolve the imports from the vista dll.
    
    [NTDLL] Don't derive appcompat version from manifest
    
    [KERNEL32] Export more Vista APIs
    
    Create kernel32_vista_static library and link both kernel32_vista and kernel32 to it.
    Export some vista functions from kernel32.
    
    [NTDLL] Link rtl_vista and export some vista functions
    
    [APISETS] Improve/fix up some exports
    
    [MSVCRT] Export _vcwprintf
    
    [KERNEL32_APITEST] Hack the loader test
    
    The test is mapping ntdll using MapViewOfFile, bypassing the loader, then enumerates it's exports, and then passes the names to GetProcAddress, which is hiding some of the original exports.
    At this point we do not support this scenario.
    
    [SDK] Enable export patching
    
    [MSVCRT][CPPRT] Adjust Cxxhandler3
    
    oops

--- sdk/cmake/config.cmake
+++ sdk/cmake/config.cmake
@@ -114,3 +114,6 @@ endif()
 
 set(USE_DUMMY_PSEH FALSE CACHE BOOL
 "Whether to disable PSEH support.")
+
+set(DISABLE_EXPORT_VERSIONING FALSE CACHE BOOL
+"Whether to disable export patching based on appcompat version.")
--- sdk/cmake/msvc.cmake
+++ sdk/cmake/msvc.cmake
@@ -312,6 +312,12 @@ function(fixup_load_config _target)
     # msvc knows how to generate a load_config so no hacks here
 endfunction()
 
+if(DISABLE_EXPORT_VERSIONING)
+    set(SPEC2DEF_NOROSCOMPAT "--noroscompat")
+else()
+    set(SPEC2DEF_NOROSCOMPAT "")
+endif()
+
 function(generate_import_lib _libname _dllname _spec_file)
 
     set(_def_file ${CMAKE_CURRENT_BINARY_DIR}/${_libname}_implib.def)
@@ -320,7 +326,7 @@ function(generate_import_lib _libname _dllname _spec_file)
     # Generate the def and asm stub files
     add_custom_command(
         OUTPUT ${_asm_stubs_file} ${_def_file}
-        COMMAND native-spec2def --ms -a=${SPEC2DEF_ARCH} --implib -n=${_dllname} -d=${_def_file} -l=${_asm_stubs_file} ${CMAKE_CURRENT_SOURCE_DIR}/${_spec_file}
+        COMMAND native-spec2def ${SPEC2DEF_NOROSCOMPAT} --ms -a=${SPEC2DEF_ARCH} --implib -n=${_dllname} -d=${_def_file} -l=${_asm_stubs_file} ${CMAKE_CURRENT_SOURCE_DIR}/${_spec_file}
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_spec_file} native-spec2def)
 
     # Compile the generated asm stub file
@@ -391,7 +397,7 @@ function(spec2def _dllname _spec_file)
     # Generate exports def and C stubs file for the DLL
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_file}.def ${CMAKE_CURRENT_BINARY_DIR}/${_file}_stubs.c
-        COMMAND native-spec2def --ms -a=${SPEC2DEF_ARCH} -n=${_dllname} -d=${CMAKE_CURRENT_BINARY_DIR}/${_file}.def -s=${CMAKE_CURRENT_BINARY_DIR}/${_file}_stubs.c ${__with_relay_arg} ${__version_arg} ${CMAKE_CURRENT_SOURCE_DIR}/${_spec_file}
+        COMMAND native-spec2def ${SPEC2DEF_NOROSCOMPAT} --ms -a=${SPEC2DEF_ARCH} -n=${_dllname} -d=${CMAKE_CURRENT_BINARY_DIR}/${_file}.def -s=${CMAKE_CURRENT_BINARY_DIR}/${_file}_stubs.c ${__with_relay_arg} ${__version_arg} ${CMAKE_CURRENT_SOURCE_DIR}/${_spec_file}
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_spec_file} native-spec2def)
 
     if(__spec2def_ADD_IMPORTLIB)
