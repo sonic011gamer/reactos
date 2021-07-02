aec7683bbfb5ddc50ede9ffbd651d5adfcdda54b
Justin miller <justinmiller100@gmail.com>
2021-07-02 11:16:34 -0700

    [NTOS:KE] Implement first version of x86 KeStartAllProcesssors

--- ntoskrnl/ex/init.c
+++ ntoskrnl/ex/init.c
@@ -1559,6 +1559,11 @@ Phase1InitializationDiscard(IN PVOID Context)
         KeBootTimeBias = 0;
     }
 
+#ifdef CONFIG_SMP
+    /* Start Application Processors */
+    KeStartAllProcessors();
+#endif
+
     /* Initialize all processors */
     if (!HalAllProcessorsStarted()) KeBugCheck(HAL1_INITIALIZATION_FAILED);
 
