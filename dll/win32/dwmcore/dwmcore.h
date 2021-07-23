1ff902155f82316d7e8691af9354d7a2fe804e0e
Justin Miller <Justinmiller100@gmail.com>
2021-07-23 15:22:09 -0700

    [DWMCORE][DWMAPI][DWMREDIR] Create initial dll files

---
+++ dll/win32/dwmcore/dwmcore.h
@@ -0,0 +1,26 @@
+/*
+ * COPYRIGHT:       See COPYING in the top level directory
+ * PROJECT:         ReactOS System Libraries
+ * PURPOSE:         ReactOS Desktop Window Manager API header
+ * COPYRIGHT:       Copyright 2021 Justin Miller (justinmiller100@gmail.com)
+ */
+
+#ifndef __DWMREDIR_H
+#define __DWMREDIR_H
+
+/* INCLUDES ******************************************************************/
+
+/* C Headers */
+#include <limits.h>
+#include <stdio.h>
+
+/* PSDK/NDK Headers */
+#define WIN32_NO_STATUS
+#include <windef.h>
+#include <winbase.h>
+#include <winreg.h>
+
+#include <ndk/rtlfuncs.h>
+
+
+#endif /* __DWMREDIR_H */
