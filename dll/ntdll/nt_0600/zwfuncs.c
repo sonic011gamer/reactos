830d49cac7daab14a388baf3112c215ba8f2d584
Justin Miller <Justinmiller100@gmail.com>
2021-07-29 00:20:36 -0700

    [NTDLL][CSRSRV] add some random temporary fixes for things

---
+++ dll/ntdll/nt_0600/zwfuncs.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+
+#define WIN32_NO_STATUS
+
+#include <windef.h>
+#include <winbase.h>
+#include <winreg.h>
+#include <winuser.h>
+#include <winwlx.h>
+#include <ndk/rtltypes.h>
+#include <ndk/umfuncs.h>
+#include <ndk/rtlfuncs.h>
+
+#define NDEBUG
+#include <debug.h>
+
+NTSTATUS
+ZwQueryLicenseValue(
+    PUNICODE_STRING ValueName,
+    ULONG *Type,
+    PVOID Data,
+    ULONG DataSize,
+    ULONG *ResultDataSize)
+{
+    DPRINT1("A vista function: ZwQueryLicenseValue has been called and a hacky value has been given");
+    return STATUS_SUCCESS;
+}
