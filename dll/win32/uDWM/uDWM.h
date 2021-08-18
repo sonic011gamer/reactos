6e45daaaf838a74083b461ec260da8935c35efab
Justin Miller <justinmiller100@gmail.com>
2021-08-18 03:35:40 -0700

    [DWMCORE][UDWM][DWMREDIR] Add in stubs for the other DWM dlls

---
+++ dll/win32/uDWM/uDWM.h
@@ -0,0 +1,26 @@
+/*
+ * COPYRIGHT:       See COPYING in the top level directory
+ * PROJECT:         ReactOS System Libraries
+ * PURPOSE:         ReactOS Desktop Window Manager header
+ * COPYRIGHT:       Copyright 2021 Justin Miller (justinmiller100@gmail.com)
+ */
+
+#ifndef __UDWM_H
+#define __UDWM_H
+
+/* INCLUDES ******************************************************************/
+
+/* C Headers */
+#include <limits.h>
+#include <stdio.h>
+
+/* PSDK/NDK Headers */
+#define WIN32_NO_STATUS
+#include <windef.h>
+#include <winbase.h>
+#include <winreg.h>
+
+#include <ndk/rtlfuncs.h>
+
+
+#endif /* __DWMAPI_H */
