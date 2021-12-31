5bbb872df8b34a233a511a03660ee81dac8d907a
DarkFire01 <justinmiller100@gmail.com>
2021-12-31 00:50:09 -0800

    [MSVCRT][CPPRT] Adjust Cxxhandler3

--- sdk/lib/cpprt/i386/cpprt.s
+++ sdk/lib/cpprt/i386/cpprt.s
@@ -7,17 +7,6 @@ EXTERN &orig : PROC
 ALIAS <&alias> = <&orig>
 ENDM
 
-EXTERN _CxxHandleV8Frame@20 : PROC
-PUBLIC ___CxxFrameHandler3
-___CxxFrameHandler3:
-    push eax
-    push dword ptr [esp + 20]
-    push dword ptr [esp + 20]
-    push dword ptr [esp + 20]
-    push dword ptr [esp + 20]
-    call _CxxHandleV8Frame@20
-    ret
-
 EXTERN ___CxxFrameHandler : PROC
 PUBLIC _CallCxxFrameHandler
 _CallCxxFrameHandler:
