be1ac4f87773359159b831571dc540bdb4508064
Adam Słaboń <asaillen@protonmail.com>
2022-03-02 15:10:15 +0100

    [NTOS:MM] Allow the GDI drivers to import from ntoskrnl and HAL
    This is required for dxg.sys from XP x64 or Vista+. Since then dxg imports directly from both ntoskrnl and win32k and not through dxgthk for the latter.

--- ntoskrnl/mm/ARM3/sysldr.c
+++ ntoskrnl/mm/ARM3/sysldr.c
@@ -1067,6 +1067,8 @@ MiResolveImageReferences(IN PVOID ImageBase,
         /* We can also allow dxapi (for Windows compat, allow IRT and coverage) */
         NormalLink = NormalLink |
                      ((_strnicmp(ImportName, "win32k", sizeof("win32k") - 1)) &&
+                      (_strnicmp(ImportName, "hal", sizeof("hal") - 1)) &&
+                      (_strnicmp(ImportName, "ntoskrnl", sizeof("ntoskrnl") - 1)) && 
                       (_strnicmp(ImportName, "dxapi", sizeof("dxapi") - 1)) &&
                       (_strnicmp(ImportName, "coverage", sizeof("coverage") - 1)) &&
                       (_strnicmp(ImportName, "irt", sizeof("irt") - 1)));
