4913ea40d1fdf764011677a4d5ace454ad2907cf
Adam Słaboń <asaillen@protonmail.com>
2022-04-09 23:36:22 +0200

    [REACTX] Update for display-drivers branch

--- win32ss/reactx/ntddraw/dxeng.c
+++ win32ss/reactx/ntddraw/dxeng.c
@@ -909,7 +909,7 @@ DxEngGetRedirectionBitmap(DWORD x1)
 /************************************************************************/
 HANDLE APIENTRY DxEngLoadImage(LPWSTR pwszDriverName, ULONG ldevtype)
 {
-    return (HANDLE)EngLoadImageEx(pwszDriverName, ldevtype);
+    return (HANDLE)LDEVOBJ_pLoadDriver(pwszDriverName, ldevtype);
 }
 
 /************************************************************************/
