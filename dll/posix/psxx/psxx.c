7baf401610c6c2e7b7d262a70bf82e0647a41773
Justin Miller <justinmiller100@gmail.com>
2022-04-20 21:50:27 -0700

    [PSXX] Revive build PSXX and build stubs in the modern reactos way

---
+++ dll/posix/psxx/psxx.c
@@ -0,0 +1,21 @@
+
+/*
+ * PROJECT:     ReactOS POSIX+ Subsystem
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ *              ReactOS POSIX+ X11 Library thing...
+ * COPYRIGHT:   Justin Miller <justinmiller100@gmai.com>
+ */
+
+
+#include <windows.h>
+
+/* LIBRARY ENTRY POINT ********************************************************/
+
+BOOL
+WINAPI
+DllMain(HANDLE hDll, DWORD dwReason, LPVOID lpReserved)
+{
+	return TRUE;
+}
+
+/* EOF */
