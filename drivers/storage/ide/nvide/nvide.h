435167105ba690c6aa51b428f48906d6e1db8b43
Dmitry Borisov <di.sean@protonmail.com>
2022-06-05 12:42:23 +0600

    [ATAPI][NVIDE] Updates

---
+++ drivers/storage/ide/nvide/nvide.h
@@ -0,0 +1,14 @@
+/*
+ * PROJECT:     ReactOS nVidia IDE Minidriver
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Common header file
+ * COPYRIGHT:   Copyright 2022 Dmitry Borisov (di.sean@protonmail.com)
+ */
+
+#ifndef _NVIDE_PCH_
+#define _NVIDE_PCH_
+
+#include <ntddk.h>
+#include <ide.h>
+
+#endif /* _NVIDE_PCH_ */
