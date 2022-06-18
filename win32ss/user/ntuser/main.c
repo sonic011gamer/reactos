3665c0b558c73d05c6dd56426102ea9857414d4c
Oleg Dubinskiy <oleg.dubinskij30@gmail.com>
2022-06-18 20:35:17 +0200

    [WIN32SS][NTUSER] Load DirectX graphics driver at system startup Load DirectX graphics kernel driver (dxg.sys) by our win32k when the system starts up. Keep it always loaded in memory, like done in Windows, instead of loading only by DirectX dlls. It fixes the problem with acessing to this driver, so then we need only call DxDdEnableDirectDraw and do other stuff when DirectDraw/Direct3D is required by anyhting. In other cases, it is called from win32k PDEV functions when changing display mode (as in Windows). Since it's used by other things too, it needs to be always loaded. Otherwise, if it's not loaded, its APIs are not accessible when needed, and then execution fails. - Implement InitializeGreCSRSS() initialization routine. Call win32k!DxDdStartupDxGraphics inside it, which loads dxg.sys. Additionally, move fonts and language ID initialization into InitializeGreCSRSS() from win32k!DriverEntry, as it is implemented in Windows. My analysis confirms that DriverEntry() does not initialize fonts stuff and language ID. It's done in IntitializeGreCSRSS() instead. - Call InitializeGreCSRSS() in NtUserInitialize() main initialization routine. My analysis confirms that this is correct, regarding the Windows implementation. It fixes display mode change problem in VMWare, when a new mode fails to be applied, because when it manages DirectDraw stuff, it calls DXG routines, and therefore fails if dxg.sys isn't loaded in memory at the moment. CORE-18221

--- win32ss/user/ntuser/main.c
+++ win32ss/user/ntuser/main.c
@@ -26,7 +26,6 @@ NTSTATUS GdiThreadDestroy(PETHREAD Thread);
 
 PSERVERINFO gpsi = NULL; // Global User Server Information.
 
-USHORT gusLanguageID;
 PPROCESSINFO ppiScrnSaver;
 PPROCESSINFO gppiList = NULL;
 
@@ -1049,15 +1048,6 @@ DriverEntry(
     NT_ROF(MsqInitializeImpl());
     NT_ROF(InitTimerImpl());
     NT_ROF(InitDCEImpl());
-
-    gusLanguageID = UserGetLanguageID();
-
-    /* Initialize FreeType library */
-    if (!InitFontSupport())
-    {
-        DPRINT1("Unable to initialize font support\n");
-        return Status;
-    }
 
     return STATUS_SUCCESS;
 }
