608bbaae4b5ce4588b518d61a729d33710203243
Justin Miller <justinmiller100@gmail.com>
2022-08-31 19:33:10 -0700

    [CRYPTBASE][CLBCATQ] Create stubs

---
+++ dll/win32_vista/cryptbase/stubs.c
@@ -0,0 +1,10 @@
+/*
+ * PROJECT:     ReactOS Cryptbase
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Stub file for cryptbase
+ * COPYRIGHT:   Copyright 2022 Justin Miller <justinmiller100@gmail.com>
+ */
+
+#include "cryptbase_private.h"
+//#define NDEBUG
+#include <debug.h>
