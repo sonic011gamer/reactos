1d87fc1d6e963e95af5619343525f9236d3b64e0
Justin Miller <justinmiller100@gmail.com>
2022-08-31 19:33:10 -0700

    [CRYPTBASE][CLBCATQ] Create stubs

---
+++ dll/win32_vista/cryptbase/DllMain.c
@@ -0,0 +1,14 @@
+
+#include "cryptbase_private.h"
+
+BOOL
+WINAPI
+DllMain(HANDLE hDll,
+        DWORD dwReason,
+        LPVOID lpReserved)
+{
+    /* For now, there isn't much to do */
+    if (dwReason == DLL_PROCESS_ATTACH)
+        DisableThreadLibraryCalls(hDll);
+    return TRUE;
+}
