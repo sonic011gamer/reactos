9425819d594a989f450ad40f39c9ef06082105c2
Justin Miller <justinmiller100@gmail.com>
2022-08-31 19:33:10 -0700

    [CRYPTBASE][CLBCATQ] Create stubs

---
+++ dll/win32_vista/clbcatq/DllMain.c
@@ -0,0 +1,14 @@
+
+#include "clbcatq_private.h"
+
+BOOL
+WINAPI
+DllMain(HANDLE hDll,
+        DWORD dwReason,
+        LPVOID lpReserved)
+{
+    /* For now, there isn't much to do */
+    if (dwReason == DLL_PROCESS_ATTACH)
+        DisableThreadLibraryCalls(hDll);
+    return TRUE;
+}
---
+++ dll/win32_vista/clbcatq/stubs.c
@@ -0,0 +1,10 @@
+/*
+ * PROJECT:     ReactOS clbcatq
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Stub file for clbcatq
+ * COPYRIGHT:   Copyright 2022 Justin Miller <justinmiller100@gmail.com>
+ */
+
+#include "clbcatq_private.h"
+//#define NDEBUG
+#include <debug.h>
