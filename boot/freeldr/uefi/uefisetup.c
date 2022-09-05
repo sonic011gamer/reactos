3aaff6f85c0a1443f2cbae7290446e13d8a07229
Justin Miller <justinmiller100@gmail.com>
2022-09-04 20:41:00 -0700

    [UEFI][SDK][FREELDR] Setup ISOs to be UEFI Bootable - Inital Uefi bin

---
+++ boot/freeldr/uefi/uefisetup.c
@@ -0,0 +1,18 @@
+/*
+ * PROJECT:     Freeldr UEFI Extension
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     UEFI Mach Setup
+ * COPYRIGHT:   Copyright 2022 Justin Miller <justinmiller100@gmail.com>
+ */
+
+#include <uefildr.h>
+
+#include <debug.h>
+
+BOOLEAN AcpiPresent = FALSE;
+
+VOID
+MachInit(const char *CmdLine)
+{
+
+}
