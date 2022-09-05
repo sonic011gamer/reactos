217b436f1306a47ed07eb3a9d6e8d09271c9350a
Justin Miller <justinmiller100@gmail.com>
2022-09-04 20:41:00 -0700

    [UEFI][SDK][FREELDR] Setup ISOs to be UEFI Bootable - Inital Uefi bin

---
+++ boot/freeldr/freeldr/arch/uefi/include/uefildr.h
@@ -0,0 +1,25 @@
+/*
+ * PROJECT:     Freeldr UEFI Extension
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Uefi freeldr core header
+ * COPYRIGHT:   Copyright 2022 Justin Miller <justinmiller100@gmail.com>
+ */
+
+#pragma once
+
+/* INCLUDES ******************************************************************/
+#include <freeldr.h>
+
+/* UEFI Headers */
+#include <Uefi.h>
+#include <DevicePath.h>
+#include <LoadedImage.h>
+#include <GraphicsOutput.h>
+#include <UgaDraw.h>
+#include <BlockIo.h>
+#include <Acpi.h>
+#include <GlobalVariable.h>
+#include <debug.h>
+#include <machuefi.h>
+
+VOID __cdecl BootMain(IN PCCH CmdLine);
---
+++ boot/freeldr/freeldr/arch/uefi/stubs.c
@@ -0,0 +1,36 @@
+/*
+ * PROJECT:     Freeldr UEFI Extension
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     UEFI stubs
+ * COPYRIGHT:   Copyright 2022 Justin Miller <justinmiller100@gmail.com>
+ */
+
+#include <uefildr.h>
+
+#include <debug.h>
+
+/* TODO: Handle this with custom Disk / partition setup */
+UCHAR
+DriveMapGetBiosDriveNumber(PCSTR DeviceName)
+{
+    return 0;
+}
+
+VOID
+StallExecutionProcessor(ULONG Microseconds)
+{
+
+}
+
+VOID
+NTAPI
+KeStallExecutionProcessor(ULONG Microseconds)
+{
+    StallExecutionProcessor(Microseconds);
+}
+
+USHORT
+__cdecl PxeCallApi(USHORT Segment, USHORT Offset, USHORT Service, VOID* Parameter)
+{
+    return 0;
+}
