06e514604b3a1c1eb799bf8c336393ddd78ad14c
George Bișoc <george.bisoc@reactos.org>
2022-10-26 21:36:02 +0200

    [FREELDR] Implement SYSTEM hive recovery at bootloader level & use CmCheckRegistry for registry validation
    Validate the SYSTEM hive with CmCheckRegistry and purge volatile data with the same function when initializing a hive descriptor for SYSTEM.
    Also implement SYSTEM recovery code that takes use of SYSTEM log in case something is fishy with the hive.
    
    If FreeLdr repairs the hive, it will mark it with HBOOT_BOOT_RECOVERED on BootRecover field of the header. All the recovered data that is present as dirty in memory will have to be flushed by the kernel once it is in charge of the system.
    
    === NOTE ===
    
    The implemented code in FreeLdr made the bootloader grew up in binary size and that prevents the booting of x64 ROS because of a check failure against the PE image of FreeLdr. As a workaround most of
    the implemented code is disabled for compilation in AMD64 for now, until a real fix comes up.

--- boot/freeldr/freeldr/ntldr/registry.c
+++ boot/freeldr/freeldr/ntldr/registry.c
@@ -2,6 +2,7 @@
  *  FreeLoader
  *
  *  Copyright (C) 2014  Timo Kreuzer <timo.kreuzer@reactos.org>
+ *                2022  George Bișoc <george.bisoc@reactos.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -41,6 +42,9 @@ HKEY CurrentControlSetKey = NULL;
 #define GET_HHIVE_FROM_HKEY(hKey)       GET_HHIVE(CmSystemHive)
 #define GET_CM_KEY_NODE(hHive, hKey)    ((PCM_KEY_NODE)HvGetCell(hHive, HKEY_TO_HCI(hKey)))
 
+#define GET_HBASE_BLOCK(ChunkBase)      ((PHBASE_BLOCK)ChunkBase)
+#define GET_REG_ADDRESS(Reg)            ((PVOID)Reg)
+
 PVOID
 NTAPI
 CmpAllocate(
@@ -62,22 +66,47 @@ CmpFree(
     FrLdrHeapFree(Ptr, 0);
 }
 
+/**
+ * @brief
+ * Initializes a flat hive descriptor for the
+ * SYSTEM hive and validates the registry hive.
+ * Volatile data is purged during this procedure
+ * for initialization.
+ *
+ * @param[in] CmHive
+ * A pointer to a CM (in-memory) hive descriptor
+ * containing the hive descriptor to be initialized.
+ *
+ * @param[in] ChunkBase
+ * A an arbitrary pointer that points to the registry
+ * chunk base. This pointer serves as the base block
+ * containing the hive file header data.
+ *
+ * @return
+ * Returns TRUE if the SYSTEM hive has been initialized
+ * and registry data inside the hive is valid, FALSE
+ * otherwise.
+ */
 BOOLEAN
-RegImportBinaryHive(
-    _In_ PVOID ChunkBase,
-    _In_ ULONG ChunkSize)
+RegInitializeSystemHive(
+    _In_ PCMHIVE CmHive,
+    _In_ PVOID ChunkBase)
 {
     NTSTATUS Status;
-    PCM_KEY_NODE KeyNode;
+/*
+ * FIXME: Disable compilation of some parts of code for AMD64 for now,
+ * since it makes the FreeLdr binary size so large that it prevents
+ * x64 ROS from booting.
+ */
+#if !defined(_M_AMD64)
+    CM_CHECK_REGISTRY_STATUS CmStatusCode;
+#endif
 
-    TRACE("RegImportBinaryHive(%p, 0x%lx)\n", ChunkBase, ChunkSize);
-
-    /* Allocate and initialize the hive */
-    CmSystemHive = FrLdrTempAlloc(sizeof(CMHIVE), 'eviH');
-    Status = HvInitialize(GET_HHIVE(CmSystemHive),
+    /* Initialize the hive */
+    Status = HvInitialize(GET_HHIVE(CmHive),
                           HINIT_FLAT, // HINIT_MEMORY_INPLACE
                           0,
-                          0,
+                          HFILE_TYPE_PRIMARY,
                           ChunkBase,
                           CmpAllocate,
                           CmpFree,
@@ -89,10 +118,360 @@ RegImportBinaryHive(
                           NULL);
     if (!NT_SUCCESS(Status))
     {
+        return FALSE;
+    }
+
+/* FIXME: See the comment above */
+#if !defined(_M_AMD64)
+    /* Now check the hive and purge volatile data */
+    CmStatusCode = CmCheckRegistry(CmHive, CM_CHECK_REGISTRY_BOOTLOADER_PURGE_VOLATILES | CM_CHECK_REGISTRY_VALIDATE_HIVE);
+    if (!CM_CHECK_REGISTRY_SUCCESS(CmStatusCode))
+    {
+        return FALSE;
+    }
+#endif
+
+    return TRUE;
+}
+
+/* FIXME: See the comment above */
+#if !defined(_M_AMD64)
+/**
+ * @brief
+ * Loads the system log data for SYSTEM
+ * hive recovery.
+ *
+ * @param[out] SystemLogData
+ * An arbitrary pointer returned by the
+ * function that points to the system
+ * log data.
+ *
+ * @return
+ * Returns TRUE if the log data could
+ * be loaded, FALSE otherwise.
+ */
+BOOLEAN
+RegLoadSystemLogForRecovery(
+    _Out_ PVOID SystemLogData)
+{
+    ARC_STATUS Status;
+    ULONG FileId;
+    CHAR SystemLogPath[MAX_PATH];
+    ULONG LogFileSize;
+    FILEINFORMATION FileInfo;
+    ULONG BytesRead;
+    PVOID LogDataVirtual;
+    PVOID LogDataPhysical;
+
+    /* Build the full path to the system log */
+    RtlStringCbCopyA(SystemLogPath, sizeof(SystemLogPath), "system32\\config\\SYSTEM.LOG");
+
+    /* Open the file */
+    Status = ArcOpen(SystemLogPath, OpenReadOnly, &FileId);
+    if (Status != ESUCCESS)
+    {
+        return FALSE;
+    }
+
+    /* Get the file length */
+    Status = ArcGetFileInformation(FileId, &FileInfo);
+    if (Status != ESUCCESS)
+    {
+        ArcClose(FileId);
+        return FALSE;
+    }
+
+    /* Capture the size of the system log file */
+    LogFileSize = FileInfo.EndingAddress.LowPart;
+    if (LogFileSize == 0)
+    {
+        ArcClose(FileId);
+        return FALSE;
+    }
+
+    /* Allocate memory blocks for our log data */
+    LogDataPhysical = MmAllocateMemoryWithType(
+        MM_SIZE_TO_PAGES(LogFileSize + MM_PAGE_SIZE - 1) << MM_PAGE_SHIFT,
+        LoaderRegistryData);
+    if (LogDataPhysical == NULL)
+    {
+        ArcClose(FileId);
+        return FALSE;
+    }
+
+    /* Convert address to virtual and assign it to caller's parameter */
+    LogDataVirtual = PaToVa(LogDataPhysical);
+    SystemLogData = LogDataVirtual;
+
+    /* Read the system log now */
+    Status = ArcRead(FileId, LogDataPhysical, LogFileSize, &BytesRead);
+    if (Status != ESUCCESS)
+    {
+        ArcClose(FileId);
+        return FALSE;
+    }
+
+    ArcClose(FileId);
+    return TRUE;
+}
+
+/**
+ * @brief
+ * Copies the registry data from the log
+ * into the primary flat SYSTEM hive. This
+ * is done during recovery process of the
+ * hive.
+ *
+ * @param[in] LogBaseBlock
+ * A pointer to a base block of the log.
+ * This parameter is used to gather the
+ * clustering factor of the log.
+ *
+ * @param[in] HiveLength
+ * The hive space length of the primary
+ * SYSTEM hive. This length takes into
+ * account the whole data present in hive
+ * minus the header. This parameter is used
+ * to calculate the necessary bitmap size
+ * for data copying into the hive.
+ *
+ * @param[in] SystemHive
+ * A pointer to the SYSTEM hive. This parameter
+ * serves as the address of the registry hive
+ * which is used for data copying into the
+ * right location within the hive.
+ *
+ * @param[in] SystemLog
+ * A pointer to the SYSTEM log. This parameter
+ * serves as the address of the hive log where
+ * contiguous blocks of data is to be copied from.
+ *
+ * @return
+ * Returns TRUE if data copying has succeeded without
+ * problems, FALSE otherwise.
+ */
+BOOLEAN
+RegCopyDataFromSystemLog(
+    _In_ PHBASE_BLOCK LogBaseBlock,
+    _In_ ULONG HiveLength,
+    _In_ PVOID SystemHive,
+    _In_ PVOID SystemLog)
+{
+    RTL_BITMAP Bitmap;
+    ULONG BitmapBuffer;
+    ULONG BitmapSize;
+    ULONG Offset;
+    ULONG HeaderSize;
+    ULONG BlockIndex;
+    ULONG LastIndex;
+    ULONG Block, BlockDest;
+    ULONG DirtyVector;
+
+    /*
+     * Compute the file offset so that
+     * we can read the dirty vector
+     * and write up data to the hive after
+     * the header.
+     */
+    HeaderSize = ROUND_UP(HV_LOG_HEADER_SIZE, LogBaseBlock->Cluster * HSECTOR_SIZE);
+    Offset = LogBaseBlock->Cluster * HSECTOR_SIZE;
+    Offset = ROUND_UP(Offset, HeaderSize);
+
+    /* Now check the dirty vector signature */
+    DirtyVector = (ULONG_PTR)SystemLog + Offset;
+    Offset += sizeof(HV_LOG_DIRTY_SIGNATURE);
+    if (DirtyVector != HV_LOG_DIRTY_SIGNATURE)
+    {
+        return FALSE;
+    }
+
+    /* Build the bitmap */
+    BitmapSize = HiveLength / HSECTOR_SIZE;
+    BitmapBuffer = (ULONG_PTR)SystemLog + Offset;
+    RtlInitializeBitMap(&Bitmap, &BitmapBuffer, BitmapSize);
+
+    /*
+     * Now read each data block individually from the
+     * log and write it back to the SYSTEM hive.
+     */
+    BlockIndex = 0;
+    Offset += BitmapSize / 8;
+    Offset = ROUND_UP(Offset, LogBaseBlock->Cluster * HSECTOR_SIZE);
+    while (BlockIndex < BitmapSize)
+    {
+        /* Check if the block is clean or we're past the last block */
+        LastIndex = BlockIndex;
+        BlockIndex = RtlFindSetBits(&Bitmap, 1, BlockIndex);
+        if (BlockIndex == ~HV_CLEAN_BLOCK || BlockIndex < LastIndex)
+        {
+            break;
+        }
+
+        /* Get the block and copy it to hive */
+        Block = (ULONG_PTR)SystemLog + Offset;
+        BlockDest = (ULONG_PTR)SystemHive + HBLOCK_SIZE + BlockIndex * HBLOCK_SIZE;
+        RtlCopyMemory(&BlockDest, &Block, HiveLength);
+
+        /* Advance to the next block to copy */
+        Offset += HiveLength;
+        BlockIndex++;
+    }
+
+    return TRUE;
+}
+
+/**
+ * @brief
+ * Recovers the SYSTEM hive from
+ * corruption.
+ *
+ * @param[in] ChunkBase
+ * An arbitrary pointer that points to the
+ * registry chunk base of the SYSTEM hive
+ * that is to be recovered in question.
+ *
+ * @param[in] SystemLog
+ * The address of the SYSTEM hive log where
+ * recovery data is to be copied from. This
+ * parameter is assumed that it has a valid
+ * base block header.
+ *
+ * @return
+ * Returns TRUE if hive recovery has succeeded,
+ * FALSE otherwise.
+ *
+ * @remarks
+ * FreeLdr only performs SYSTEM hive recovery in
+ * memory, the healed hive data is still not present
+ * physically on the disk yet. The kernel is responsible
+ * to sync all the dirty data from memory to the backing
+ * storage in order to make the recovery data visible.
+ */
+BOOLEAN
+RegRecoverSystemHive(
+    _Inout_ PVOID ChunkBase,
+    _In_ ULONG_PTR SystemLog)
+{
+    PHBASE_BLOCK SystemHiveBlock, SystemLogBlock;
+    PVOID SystemFlatHive, SystemFlatLog;
+
+    /* Capture the base blocks */
+    SystemHiveBlock = GET_HBASE_BLOCK(ChunkBase);
+    SystemLogBlock = GET_HBASE_BLOCK(SystemLog);
+
+    /* Validate the base block */
+    if (!HvpVerifyHiveHeader(SystemHiveBlock, HFILE_TYPE_PRIMARY))
+    {
+        /* Recover it */
+        RtlCopyMemory(SystemHiveBlock,
+                      SystemLogBlock,
+                      SystemLogBlock->Cluster * HSECTOR_SIZE);
+        SystemHiveBlock->Type = HFILE_TYPE_PRIMARY;
+    }
+
+    /* Copy data from the SYSTEM log */
+    SystemFlatHive = GET_REG_ADDRESS(ChunkBase);
+    SystemFlatLog = GET_REG_ADDRESS(SystemLog);
+    if (!RegCopyDataFromSystemLog(SystemLogBlock,
+                                  SystemHiveBlock->Length,
+                                  SystemFlatHive,
+                                  SystemFlatLog))
+    {
+        return FALSE;
+    }
+
+    /* Fix the secondary sequence and set up a new checksum */
+    SystemHiveBlock->Sequence2 = SystemHiveBlock->Sequence1;
+    SystemHiveBlock->CheckSum = HvpHiveHeaderChecksum(SystemHiveBlock);
+    return TRUE;
+}
+#endif
+
+/**
+ * @brief
+ * Imports the SYSTEM binary hive from
+ * the registry base chunk that's been
+ * provided by the loader block.
+ *
+ * @param[in] ChunkBase
+ * A pointer to the registry base chunk
+ * that serves for SYSTEM hive initialization.
+ *
+ * @param[in] ChunkSize
+ * The size of the registry base chunk. This
+ * parameter refers to the actual size of
+ * the SYSTEM hive. This parameter is currently
+ * unused.
+ *
+ * @return
+ * Returns TRUE if hive importing and initialization
+ * have succeeded, FALSE otherwise.
+ */
+BOOLEAN
+RegImportBinaryHive(
+    _In_ PVOID ChunkBase,
+    _In_ ULONG ChunkSize)
+{
+    BOOLEAN Success;
+/* FIXME: See the comment above */
+#if !defined(_M_AMD64)
+    ULONG_PTR SystemLog;
+#endif
+    PCM_KEY_NODE KeyNode;
+
+    TRACE("RegImportBinaryHive(%p, 0x%lx)\n", ChunkBase, ChunkSize);
+
+    /* Assume that we don't need boot recover, unless we have to */
+    ((PHBASE_BLOCK)ChunkBase)->BootRecover = HBOOT_NO_BOOT_RECOVER;
+
+    /* Allocate and initialize the hive */
+    CmSystemHive = FrLdrTempAlloc(sizeof(CMHIVE), 'eviH');
+    Success = RegInitializeSystemHive(CmSystemHive, ChunkBase);
+    if (!Success)
+/* FIXME: See the comment above */
+#if defined(_M_AMD64)
+    {
         ERR("Corrupted hive %p!\n", ChunkBase);
         FrLdrTempFree(CmSystemHive, 'eviH');
         return FALSE;
     }
+#else
+    {
+        /* Free the buffer and retry again */
+        FrLdrTempFree(CmSystemHive, 'eviH');
+        CmSystemHive = NULL;
+
+        /* Get the SYSTEM log address */
+        if (!RegLoadSystemLogForRecovery(&SystemLog))
+        {
+            ERR("Couldn't get the SYSTEM log data\n");
+            return FALSE;
+        }
+
+        /* Now recover the SYSTEM hive */
+        if (!RegRecoverSystemHive(ChunkBase, SystemLog))
+        {
+            ERR("SYSTEM hive recovery failed\n");
+            return FALSE;
+        }
+
+        /* Now retry initializing the hive again */
+        CmSystemHive = FrLdrTempAlloc(sizeof(CMHIVE), 'eviH');
+        Success = RegInitializeSystemHive(CmSystemHive, ChunkBase);
+        if (!Success)
+        {
+            ERR("Corrupted hive (despite recovery) %p\n", ChunkBase);
+            FrLdrTempFree(CmSystemHive, 'eviH');
+            return FALSE;
+        }
+
+        /*
+         * Acknowledge the kernel we recovered
+         * the SYSTEM hive on our side.
+         */
+        ((PHBASE_BLOCK)ChunkBase)->BootRecover = HBOOT_BOOT_RECOVERED;
+    }
+#endif
 
     /* Save the root key node */
     SystemHive = GET_HHIVE(CmSystemHive);
