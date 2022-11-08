fe192faedeae9549344a8e3d7e06bd83fe81b70e
Justin Miller <justinmiller100@gmail.com>
2022-11-08 15:22:16 -0800

    [KERNEL32] K32 Loaded patch

--- dll/win32/kernel32/kernel32_vista/k32_vista.h
+++ dll/win32/kernel32/kernel32_vista/k32_vista.h
@@ -12,11 +12,24 @@
 
 #define NTOS_MODE_USER
 #include <ndk/iofuncs.h>
-#include <ndk/kefuncs.h>
 #include <ndk/obfuncs.h>
 #include <ndk/psfuncs.h>
 #include <ndk/rtlfuncs.h>
-
+#include <ndk/cmfuncs.h>
+#include <ndk/exfuncs.h>
+#include <ndk/iotypes.h>
+#include <ndk/kdtypes.h>
+#include <ndk/kefuncs.h>
+#include <ndk/ketypes.h>
+#include <ndk/ldrfuncs.h>
+#include <ndk/mmfuncs.h>
+#include <ndk/mmtypes.h>
+#include <ndk/obfuncs.h>
+#include <ndk/psfuncs.h>
+#include <ndk/pstypes.h>
+#include <ndk/rtlfuncs.h>
+#include <ndk/setypes.h>
+#include <ndk/umfuncs.h>
 /* CSRSS Headers */
 #include <win/base.h>
 
