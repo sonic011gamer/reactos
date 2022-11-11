38c6ca69d24e36e10a989d6ccb4d71dcf1a52e10
Justin Miller <justinmiller100@gmail.com>
2022-11-11 15:08:57 -0800

    Create seom stubs for now

---
+++ dll/directx/wine/dxva2/stubs.c
@@ -0,0 +1,26 @@
+#define WIN32_NO_STATUS
+#define _INC_WINDOWS
+#define COM_NO_WINDOWS_H
+#include <precomp.h>
+
+#define NDEBUG
+#include <reactos/debug.h>
+
+
+HRESULT DXVA2CreateDirect3DDeviceManager9(
+  UINT                    *pResetToken,
+  PVOID **ppDeviceManager
+)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+HRESULT DXVA2CreateVideoService(
+  ULONG_PTR *pDD,
+  REFIID           riid,
+  void             **ppService
+)
+{
+  return 0;
+}
