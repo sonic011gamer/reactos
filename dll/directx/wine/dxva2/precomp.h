4162aaf995da300017d08c1276376d8e261b1f59
Justin Miller <justinmiller100@gmail.com>
2022-11-11 15:08:57 -0800

    Create seom stubs for now

---
+++ dll/directx/wine/dxva2/precomp.h
@@ -0,0 +1,20 @@
+
+#ifndef _dxva2_PCH_
+#define _dxva2_PCH_
+
+#include <stdarg.h>
+
+#define WIN32_NO_STATUS
+#define _INC_WINDOWS
+#define COM_NO_WINDOWS_H
+
+#define COBJMACROS
+
+#include <windef.h>
+#include <winbase.h>
+#include <wingdi.h>
+#include <objbase.h>
+
+#include <wine/debug.h>
+
+#endif /* _dxva2_PCH_ */
