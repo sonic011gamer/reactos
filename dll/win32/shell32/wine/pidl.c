d187d780f6c6bc269692a31f1874123a8535c711
Shilin Victor <chrono.monochrome@gmail.com>
2023-03-05 23:42:50 +0000

    [SHELL32] patch by Oleg Dubinskiy

--- dll/win32/shell32/wine/pidl.c
+++ dll/win32/shell32/wine/pidl.c
@@ -1482,7 +1482,7 @@ HRESULT WINAPI SHGetNameFromIDList(PCIDLIST_ABSOLUTE pidl, SIGDN sigdnName, PWST
     return ret;
 }
 
-#ifndef __REACTOS__
+//#ifndef __REACTOS__
 
 /*************************************************************************
  * SHGetIDListFromObject             [SHELL32.@]
@@ -1560,7 +1560,7 @@ HRESULT WINAPI SHGetIDListFromObject(IUnknown *punk, PIDLIST_ABSOLUTE *ppidl)
     return ret;
 }
 
-#endif /* !__REACTOS__ */
+//#endif /* !__REACTOS__ */
 
 /**************************************************************************
  *
