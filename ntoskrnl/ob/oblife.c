5ddbfe72035585add8afe636b95f2a9ff1ef7414
Shilin Victor <chrono.monochrome@gmail.com>
2023-03-07 13:29:45 +0000

    [REACTOS_VISTA][NTOSKRNL] This is soley for NT6+ kernel funcs
    this only includes new functions for NT 6.1, no logic change in any existing code.

--- ntoskrnl/ob/oblife.c
+++ ntoskrnl/ob/oblife.c
@@ -1963,4 +1963,12 @@ NtSetInformationObject(IN HANDLE ObjectHandle,
     return Status;
 }
 
+POBJECT_TYPE
+NTAPI
+ObGetObjectType(IN PVOID Object)
+{
+    UNIMPLEMENTED;
+    return NULL;
+}
+
 /* EOF */
