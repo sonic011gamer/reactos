73ce11a466385d18a1126db5c9721ed68b2702b9
Justin Miller <Justinmiller100@gmail.com>
2023-03-27 12:39:52 -0700

    [WIN32SS_VISTA] Begin importing LDDM headers and adjusting them for ros

---
+++ win32ss_vista/include/locd3d.h
@@ -0,0 +1,11 @@
+/*
+ * PROJECT:     ReactOS WDDM Alternative Headers
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     WDDM Headers
+ * COPYRIGHT:   Copyright 2023 Justin Miller <justinmiller100@gmail.com>
+ */
+#pragma once
+
+#include "d3dkmddi.h"
+#include "d3dkmthk.h"
+#include "dispmprt.h"
