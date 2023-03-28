18be0c73ce7787562fcca3737dd26ccd5a5ac85a
Justin Miller <JUstinmiller100@gmail.com>
2023-03-27 17:56:35 -0700

    [BOOT][SDK][REACTOS_VISTA] Switch to NT 6.1 but keep code as 5.2

--- boot/freeldr/freeldr/ntldr/setupldr.c
+++ boot/freeldr/freeldr/ntldr/setupldr.c
@@ -773,7 +773,7 @@ LoadReactOSSetup(
         UiResetForSOS();
 
     /* Allocate and minimally-initialize the Loader Parameter Block */
-    AllocateAndInitLPB(_WIN32_WINNT_WS03, &LoaderBlock);
+    AllocateAndInitLPB(TARGET_VERSION, &LoaderBlock);
 
     /* Allocate and initialize the setup loader block */
     SetupBlock = &WinLdrSystemBlock->SetupBlock;
@@ -811,7 +811,7 @@ LoadReactOSSetup(
     UiDrawStatusText("The Setup program is starting...");
 
     /* Finish loading */
-    return LoadAndBootWindowsCommon(_WIN32_WINNT_WS03,
+    return LoadAndBootWindowsCommon(TARGET_VERSION,
                                     LoaderBlock,
                                     BootOptions,
                                     BootPath);
