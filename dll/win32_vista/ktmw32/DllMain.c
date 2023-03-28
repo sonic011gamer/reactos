5f9847ab06188f22116503eded3a384144e3d9b8
Justin Miller <Justinmiller100@gmail.com>
2023-03-28 10:36:09 -0700

    [WIN32_VISTA] Add even more stub Dlls - Import ncrypt from wine

---
+++ dll/win32_vista/ktmw32/DllMain.c
@@ -0,0 +1,19 @@
+
+
+#include "KB.h"
+
+
+BOOL
+WINAPI
+DllMain(HANDLE hDll,
+        DWORD dwReason,
+        LPVOID lpReserved)
+{
+
+    return TRUE;
+}
+
+BOOL WINAPI DllMainCRTStartup(HANDLE hinstDll, DWORD fdwReason, LPVOID fImpLoad)
+{
+  return TRUE;
+}
