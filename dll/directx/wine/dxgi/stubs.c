32c8be0e06a80e118ad8bcedb768f2ca35dd2710
Justin Miller <Justinmiller100@gmail.com>
2023-03-28 20:21:10 -0700

    [REACTOS_VISTA][DWRITE] Import vista+ wine DLLs ... compile dwrite

---
+++ dll/directx/wine/dxgi/stubs.c
@@ -0,0 +1,12 @@
+#include <dxgi_private.h>
+
+HRESULT WINAPI CreateDXGIFactory(REFIID riid, void **factory)
+{
+    return 0;
+}
+
+
+HRESULT WINAPI CreateDXGIFactory1(REFIID riid, void **factory)
+{
+    return 0;
+}
