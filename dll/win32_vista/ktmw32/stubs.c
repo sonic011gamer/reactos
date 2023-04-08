589f41015f77f6aa567622b3c576c52ed6f5c91e
Justin Miller <Justinmiller100@gmail.com>
2023-04-07 21:03:32 -0700

    [WIN32_VISTA:KTMW32] CreateTransaction stub - oh god, this is a syscall

--- dll/win32_vista/ktmw32/stubs.c
+++ dll/win32_vista/ktmw32/stubs.c
@@ -1,2 +1,23 @@
 #include "KB.h"
 #include <debug.h>
+
+HANDLE WINAPI CreateTransaction( SECURITY_ATTRIBUTES *sa, GUID *guid, DWORD options, DWORD level, DWORD flags,
+        DWORD timeout, WCHAR *desc )
+{
+    ULONG obj_flags = OBJ_CASE_INSENSITIVE;
+    UNICODE_STRING desc_str;
+    OBJECT_ATTRIBUTES attr;
+    //LARGE_INTEGER time;
+   // HANDLE handle;
+
+    if (sa && sa->bInheritHandle) obj_flags |= OBJ_INHERIT;
+    InitializeObjectAttributes( &attr, NULL, obj_flags, 0, sa ? sa->lpSecurityDescriptor : NULL );
+
+    RtlInitUnicodeString( &desc_str, desc );
+#if 0
+     oh god this is a vista+ syscall, yeah that aint happening
+    NtCreateTransaction( &handle, 0 /* FIXME */, &attr, guid, NULL, options, level, flags,
+            100, &desc_str );
+#endif
+    return NULL;
+}
