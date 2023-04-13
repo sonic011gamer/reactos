6daec0bc693b89d4ff6342de5fe72bb86f931b5f
Justin Miller <Justinmiller100@gmail.com>
2023-04-13 06:02:41 -0700

    [WIN32SS_VISTA:DISPLIB:DXGKRNL:MINIPORT] WDDM Bringup v0.1

---
+++ win32ss_vista/dxgkrnl/interface_vista.c
@@ -0,0 +1,249 @@
+/*
+ * PROJECT:     ReactOS Display Driver Model
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     DXGKRNL Vista/7 Interface APIs
+ * COPYRIGHT:   Copyright 2023 Justin Miller <justinmiller100@gmail.com>
+ */
+
+#include <dxgkrnl.h>
+//#define NDEBUG
+#include <debug.h>
+
+/*
+ * https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/dispmprt/nc-dispmprt-dxgkcb_eval_acpi_method
+ * @ UNIMPLEMENTED
+ */
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbEvalAcpiMethod(_In_ HANDLE DeviceHandle,
+                          _In_ ULONG DeviceUid,
+                          _In_reads_bytes_(AcpiInputSize) PACPI_EVAL_INPUT_BUFFER_COMPLEX AcpiInputBuffer,
+                          _In_range_(>=, sizeof(ACPI_EVAL_INPUT_BUFFER_COMPLEX)) ULONG AcpiInputSize,
+                          _Out_writes_bytes_(AcpiOutputSize) PACPI_EVAL_OUTPUT_BUFFER AcpiOutputBuffer,
+                          _In_range_(>=, sizeof(ACPI_EVAL_OUTPUT_BUFFER)) ULONG AcpiOutputSize)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+/*
+ * https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/dispmprt/nc-dispmprt-dxgkcb_get_device_information
+ * @ UNIMPLEMENTED
+ */
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbGetDeviceInformation(_In_ HANDLE DeviceHandle,
+                                _Out_ PDXGK_DEVICE_INFO DeviceInfo)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+/*
+ * https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/dispmprt/nc-dispmprt-dxgkcb_indicate_child_status
+ * @ UNIMPLEMENTED
+ */
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbIndicateChildStatus(_In_ HANDLE DeviceHandle,
+                               _In_ PDXGK_CHILD_STATUS ChildStatus)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbMapMemory(_In_ HANDLE DeviceHandle,
+                     _In_ PHYSICAL_ADDRESS TranslatedAddress,
+                     _In_ ULONG Length,
+                     _In_ BOOLEAN InIoSpace,
+                     _In_ BOOLEAN MapToUserMode,
+                     _In_ MEMORY_CACHING_TYPE CacheType,
+                     _Outptr_ PVOID *VirtualAddress)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+BOOLEAN
+APIENTRY
+RDDM_DxgkCbQueueDpc(_In_ HANDLE DeviceHandle)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return FALSE;
+}
+
+
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbQueryServices(_In_ HANDLE DeviceHandle,
+                         _In_ DXGK_SERVICES ServicesType,
+                         _Inout_ PINTERFACE Interface)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbReadDeviceSpace(_In_ HANDLE DeviceHandle,
+                           _In_ ULONG DataType,
+                           _Out_writes_bytes_to_(Length, *BytesRead) PVOID Buffer,
+                           _In_ ULONG Offset,
+                           _In_ ULONG Length,
+                           _Out_ PULONG BytesRead)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbSynchronizeExecution(_In_ HANDLE DeviceHandle,
+                                _In_ PKSYNCHRONIZE_ROUTINE SynchronizeRoutine,
+                                _In_ PVOID Context,
+                                _In_ ULONG MessageNumber,
+                                _Out_ PBOOLEAN ReturnValue)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbUnmapMemory(_In_ HANDLE DeviceHandle,
+                       _In_ PVOID VirtualAddress)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbWriteDeviceSpace(_In_ HANDLE DeviceHandle,
+                            _In_ ULONG DataType,
+                            _In_reads_bytes_(Length) PVOID Buffer,
+                            _In_ ULONG Offset,
+                            _In_ ULONG Length,
+                            _Out_ _Out_range_(<=, Length) PULONG BytesWritten)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+NTSTATUS
+APIENTRY
+RDDM_DxgkCbIsDevicePresent(_In_ HANDLE DeviceHandle,
+                           _In_ PPCI_DEVICE_PRESENCE_PARAMETERS DevicePresenceParameters,
+                           _Out_ PBOOLEAN DevicePresent)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+VOID*
+APIENTRY
+CALLBACK
+RDDM_DxgkCbGetHandleData(IN_CONST_PDXGKARGCB_GETHANDLEDATA GetHandleData)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return NULL;
+}
+
+D3DKMT_HANDLE
+APIENTRY
+CALLBACK
+RDDM_DxgkCbGetHandleParent(IN_D3DKMT_HANDLE hAllocation)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+D3DKMT_HANDLE
+APIENTRY
+CALLBACK
+RDDM_DxgkCbEnumHandleChildren(IN_CONST_PDXGKARGCB_ENUMHANDLECHILDREN EnumHandleChildren)
+{
+        //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+VOID
+APIENTRY
+CALLBACK
+RDDM_DxgkCbNotifyInterrupt(IN_CONST_HANDLE hAdapter,
+                           IN_CONST_PDXGKARGCB_NOTIFY_INTERRUPT_DATA NotifyInterruptData)
+{
+   //TODO: Implement meh
+    UNIMPLEMENTED;
+}
+
+VOID
+APIENTRY
+CALLBACK
+RDDM_DxgkCbNotifyDpc(IN_CONST_HANDLE hAdapter)
+{
+   //TODO: Implement meh
+    UNIMPLEMENTED;
+}
+
+NTSTATUS
+APIENTRY
+CALLBACK
+RDDM_DxgkCbQueryVidPnInterface(IN_CONST_D3DKMDT_HVIDPN                hVidPn,
+                               IN_CONST_DXGK_VIDPN_INTERFACE_VERSION  VidPnInterfaceVersion,
+                               DEREF_OUT_CONST_PPDXGK_VIDPN_INTERFACE ppVidPnInterface)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+NTSTATUS
+APIENTRY
+CALLBACK
+RDDM_DxgkCbQueryMonitorInterface(IN_CONST_HANDLE                          hAdapter,
+                                 IN_CONST_DXGK_MONITOR_INTERFACE_VERSION  MonitorInterfaceVersion,
+                                 DEREF_OUT_CONST_PPDXGK_MONITOR_INTERFACE ppMonitorInterface)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+NTSTATUS
+APIENTRY
+CALLBACK
+RDDM_DxgkCbGetCaptureAddress(INOUT_PDXGKARGCB_GETCAPTUREADDRESS GetCaptureAddress)
+{
+    //TODO: Implement meh
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+VOID
+APIENTRY
+RDDM_DxgkCbLogEtwEvent(_In_ CONST LPCGUID EventGuid,
+                       _In_ UCHAR Type,
+                       _In_ USHORT EventBufferSize,
+                       _In_reads_bytes_(EventBufferSize) PVOID EventBuffer)
+{
+   //TODO: Implement meh
+    UNIMPLEMENTED;
+}
