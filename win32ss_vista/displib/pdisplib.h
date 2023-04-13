ea22a28b9cee53bb56326eb118231a1c8c1d14ae
Justin Miller <Justinmiller100@gmail.com>
2023-04-13 06:02:41 -0700

    [WIN32SS_VISTA:DISPLIB:DXGKRNL:MINIPORT] WDDM Bringup v0.1

--- win32ss_vista/displib/pdisplib.h
+++ win32ss_vista/displib/pdisplib.h
@@ -1,17 +1,20 @@
 /*
  * PROJECT:     ReactOS Display Driver Model
  * LICENSE:     MIT (https://spdx.org/licenses/MIT)
- * PURPOSE:     Core Header
+ * PURPOSE:     DISPLIB static library
  * COPYRIGHT:   Copyright 2023 Justin Miller <justinmiller100@gmail.com>
  */
 
-#pragma once
-
 #include <ntddk.h>
 #include <windef.h>
-#include <ntstatus.h>
-
+#include <winerror.h>
 #include <stdio.h>
 #include <locd3d.h>
+#include <wdm.h>
 
-#include "pdxgkrnl.h"
+
+NTSTATUS
+RDDM_LoadDxgkrnl(FILE_OBJECT **FileObject, DEVICE_OBJECT **DeviceObject);
+
+VOID
+RDDM_SetupIoControlReq(DEVICE_OBJECT *DeviceObject);
