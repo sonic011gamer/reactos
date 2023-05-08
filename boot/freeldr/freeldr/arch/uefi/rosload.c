5e10165d841014e21fbb664677f465fb83308a65
Justin Miller <Justinmiller100@gmail.com>
2023-05-08 05:21:00 -0700

    implement freeldr rosload`

---
+++ boot/freeldr/freeldr/arch/uefi/rosload.c
@@ -0,0 +1,46 @@
+#include <uefildr.h>
+
+#include "../../../../environ/include/bl.h"
+#include <debug.h>
+
+extern EFI_HANDLE GlobalImageHandle;
+extern EFI_SYSTEM_TABLE *GlobalSystemTable;
+
+
+/*
+ *
+ * Both of these para   meters to EFI_ENTRY are pointers.
+ * awkwardly, so is BlAppicationEntry.
+ * So behold the birth of the weirdest fucking idea Illen has ever given me
+ * Freeldr.epic
+ */
+EFI_STATUS
+OslMain(_In_ PBOOT_APPLICATION_PARAMETER_BLOCK BootAppParameters,
+	    _In_ PBL_LIBRARY_PARAMETERS LibraryParameters)
+{
+    PBL_LIBRARY_PARAMETERS	LibraryParams;
+	PBL_FIRMWARE_DESCRIPTOR FirmwareDescriptor;
+    UINTN ParamPointer;
+
+	if (!BootAppParameters)
+	{
+	    // Invalid parameter
+	    return STATUS_INVALID_PARAMETER;
+	}
+
+	LibraryParams = LibraryParameters;
+	ParamPointer = (UINTN) BootAppParameters;
+	FirmwareDescriptor = (PBL_FIRMWARE_DESCRIPTOR) (ParamPointer + BootAppParameters->FirmwareParametersOffset);
+
+	// Do what ever you want now
+    if (FirmwareDescriptor->SystemTable)
+	{
+            GlobalImageHandle = FirmwareDescriptor->ImageHandle;
+			GlobalSystemTable = FirmwareDescriptor->SystemTable;
+	}
+    /* Some idiot decided to boot freeldr as a bootmgr app*/
+    GlobalSystemTable->ConOut->OutputString(GlobalSystemTable->ConOut,L"Starting Freeldr from bootmgr\n");
+
+    BootMain(NULL);
+    return 0;
+}
