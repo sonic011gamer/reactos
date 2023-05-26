c80e7826fabce9a49e9c4bc5ba8c42c92165b1a3
Justin Miller <Justinmiller100@gmail.com>
2023-05-26 08:23:28 -0700

    [SHELL32] Stub Association support - Introduced vista RTM

---
+++ dll/win32/shell32/Assoc.cpp
@@ -0,0 +1,62 @@
+/*
+ * PROJECT:     shell32
+ * LICENSE:     LGPL-2.1-or-later (https://spdx.org/licenses/LGPL-2.1-or-later)
+ * PURPOSE:     Assoc
+ * COPYRIGHT:   Copyright 2023 Justin Miller <justinmiller100@gmail.com>
+ */
+
+#include "precomp.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(shell_ad);
+
+/*
+ *
+ *  UNIMPLEMENTED
+ *  https://www.geoffchappell.com/studies/windows/shell/shell32/api/assocelem/createelement.htm?tx=57,63
+ */
+HRESULT
+WINAPI
+AssocCreateElement (
+    REFCLSID rclsid,
+    REFIID riid,
+    PVOID *ppv)
+{
+    return E_FAIL;
+}
+
+/*
+ *
+ *  UNIMPLEMENTED
+ *  https://www.geoffchappell.com/studies/windows/shell/shell32/api/assocutil/getkeys.htm
+ */
+
+UINT
+WINAPI
+SHGetAssocKeys (
+    IUnknown *punkAssociationInfo,
+    HKEY *aKeys,
+    UINT cKeys)
+{
+        UNIMPLEMENTED;
+    __debugbreak();
+    return 0;
+}
+
+/*
+ *
+ *  UNIMPLEMENTED
+ *  https://www.geoffchappell.com/studies/windows/shell/shell32/api/assocutil/getproplistforext.htm
+ */
+
+HRESULT
+WINAPI
+AssocGetPropListForExt (
+    PCWSTR pszAssoc,
+    PROPERTYKEY *pkey,
+    REFIID riid,
+    PVOID *ppv)
+{
+    UNIMPLEMENTED;
+    __debugbreak();
+    return E_FAIL;
+}
