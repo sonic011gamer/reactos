5f0df36b2746fd3274a80b68a30ff597737b81d7
Justin Miller <Justinmiller100@gmail.com>
2023-06-22 22:40:10 -0700

    [DXGKRNL] HALF-IMPLEMENT DpiInitialize

--- win32ss_vista/dxgkrnl/dxgkrnl.c
+++ win32ss_vista/dxgkrnl/dxgkrnl.c
@@ -9,15 +9,119 @@
 //#define NDEBUG
 #include <debug.h>
 
-/* This function is the first real thing that happens to interact with the WDDM Miniport */
+/**
+ * @brief Setups all handles for the needed IRPs and passes the Miniports callback list into a internal strucutre
+ *
+ * @param DriverObject
+ *
+ * @param SourceString
+ *
+ * @param DriverInitData The callback list provided by a WDDM miniport driver
+ *
+ * @return NTSTATUS
+ *
+ *  HALF-IMPLEMENTED
+ */
 NTSTATUS
 NTAPI
 DpiInitialize(PDRIVER_OBJECT DriverObject, PCUNICODE_STRING SourceString, DRIVER_INITIALIZATION_DATA *DriverInitData)
 {
+    NTSTATUS Status;
+    PDRIVER_EXTENSION DriverExtend;
+    PDXGKRNL_PRIVATE_EXTENSION DriverObjectExtension;
+
+    /*
+     * Okay so clearly this func does quite a few annoying things so why is it half implemented? you may ask if you're looking at this code
+     * (Though why are you lol these notes are mostly for me to come back and fix crap)
+     */
+
+    /*
+     * Windows Vista/7, Hell Even 8 (Though that's a bigger can of beans) validate it's API versions here and also has a extra later of protection
+     * This should be defined as a FIXME: In windows a WDDM minport driver can vary in size, not filling out every callback in the list.
+     * Windows does a validation to make sure a specific set of these callbacks are implemented. On top of this the DxgkDdi.. Call backs seem to be
+     * implemented at the very end of the internal struct. The IoALlocateDriverObjectExtension call will be fed the maximum size. However DXGKNRL will keep track
+     * of the implemented call back size
+     *
+     * On top of the above some basic global structures are setup here that I don't fully understand what they do yet. Overall this API works
+     * I Just am not matching ALL of window's behaviors yet,
+     */
+
     DPRINT1("DpiInitialize: Entry Point\n");
+    /* This function is the first real thing that happens to interact with the WDDM Miniport */
     DPRINT1("DpiInitialize: Wddm Miniport driver Reports version: 0x%X\n", DriverInitData->Version);
 
-    PDRIVER_EXTENSION DriverExtend;
+    Status = IoAllocateDriverObjectExtension(DriverObject, DriverObject, sizeof(DXGKRNL_PRIVATE_EXTENSION), &DriverObjectExtension);
+
+    /* Fill out the internal structure - WIP */
+    DriverObjectExtension->DriverInitData = DriverInitData;
+    DriverObjectExtension->DriverObject = DriverObject;
+
+    /* Fill out the call backs - This can be done in a way that's not cursed but i wanna see them all visible for now - mostly for testing */
+    DriverObjectExtension->DxgkDdiAddDevice = DriverInitData-> DxgkDdiAddDevice;
+    DriverObjectExtension->DxgkDdiStartDevice = DriverInitData-> DxgkDdiStartDevice;
+    DriverObjectExtension->DxgkDdiAddDevice = DriverInitData-> DxgkDdiAddDevice;
+    DriverObjectExtension->DxgkDdiStartDevice = DriverInitData-> DxgkDdiStartDevice;
+    DriverObjectExtension->DxgkDdiStopDevice = DriverInitData-> DxgkDdiStopDevice;
+    DriverObjectExtension->DxgkDdiRemoveDevice = DriverInitData-> DxgkDdiRemoveDevice;
+    DriverObjectExtension->DxgkDdiDispatchIoRequest = DriverInitData-> DxgkDdiDispatchIoRequest;
+    DriverObjectExtension->DxgkDdiInterruptRoutine = DriverInitData-> DxgkDdiInterruptRoutine;
+    DriverObjectExtension->DxgkDdiDpcRoutine = DriverInitData-> DxgkDdiDpcRoutine;
+    DriverObjectExtension->DxgkDdiQueryChildRelations = DriverInitData-> DxgkDdiQueryChildRelations;
+    DriverObjectExtension->DxgkDdiQueryChildStatus = DriverInitData-> DxgkDdiQueryChildStatus;
+    DriverObjectExtension->DxgkDdiQueryDeviceDescriptor = DriverInitData-> DxgkDdiQueryDeviceDescriptor;
+    DriverObjectExtension->DxgkDdiSetPowerState = DriverInitData-> DxgkDdiSetPowerState;
+    DriverObjectExtension->DxgkDdiNotifyAcpiEvent = DriverInitData-> DxgkDdiNotifyAcpiEvent;
+    DriverObjectExtension->DxgkDdiResetDevice = DriverInitData-> DxgkDdiResetDevice;
+    DriverObjectExtension->DxgkDdiUnload = DriverInitData-> DxgkDdiUnload;
+    DriverObjectExtension->DxgkDdiQueryInterface = DriverInitData-> DxgkDdiQueryInterface;
+    DriverObjectExtension->DxgkDdiControlEtwLogging = DriverInitData-> DxgkDdiControlEtwLogging;
+    DriverObjectExtension->DxgkDdiQueryAdapterInfo = DriverInitData-> DxgkDdiQueryAdapterInfo;
+    DriverObjectExtension->DxgkDdiCreateDevice = DriverInitData-> DxgkDdiCreateDevice;
+    DriverObjectExtension->DxgkDdiCreateAllocation = DriverInitData-> DxgkDdiCreateAllocation;
+    DriverObjectExtension->DxgkDdiDestroyAllocation = DriverInitData-> DxgkDdiDestroyAllocation;
+    DriverObjectExtension->DxgkDdiDescribeAllocation = DriverInitData-> DxgkDdiDescribeAllocation;
+    DriverObjectExtension->DxgkDdiGetStandardAllocationDriverData = DriverInitData-> DxgkDdiGetStandardAllocationDriverData;
+    DriverObjectExtension->DxgkDdiAcquireSwizzlingRange = DriverInitData-> DxgkDdiAcquireSwizzlingRange;
+    DriverObjectExtension->DxgkDdiReleaseSwizzlingRange = DriverInitData-> DxgkDdiReleaseSwizzlingRange;
+    DriverObjectExtension->DxgkDdiPatch = DriverInitData-> DxgkDdiPatch;
+    DriverObjectExtension->DxgkDdiSubmitCommand = DriverInitData-> DxgkDdiSubmitCommand;
+    DriverObjectExtension->DxgkDdiPreemptCommand = DriverInitData-> DxgkDdiPreemptCommand;
+    DriverObjectExtension->DxgkDdiBuildPagingBuffer = DriverInitData-> DxgkDdiBuildPagingBuffer;
+    DriverObjectExtension->DxgkDdiSetPalette = DriverInitData-> DxgkDdiSetPalette;
+    DriverObjectExtension->DxgkDdiSetPointerPosition = DriverInitData-> DxgkDdiSetPointerPosition;
+    DriverObjectExtension->DxgkDdiSetPointerShape = DriverInitData-> DxgkDdiSetPointerShape;
+    DriverObjectExtension->DxgkDdiResetFromTimeout = DriverInitData-> DxgkDdiResetFromTimeout;
+    DriverObjectExtension->DxgkDdiRestartFromTimeout = DriverInitData-> DxgkDdiRestartFromTimeout;
+    DriverObjectExtension->DxgkDdiEscape = DriverInitData-> DxgkDdiEscape;
+    DriverObjectExtension->DxgkDdiCollectDbgInfo = DriverInitData-> DxgkDdiCollectDbgInfo;
+    DriverObjectExtension->DxgkDdiQueryCurrentFence = DriverInitData-> DxgkDdiQueryCurrentFence;
+    DriverObjectExtension->DxgkDdiIsSupportedVidPn = DriverInitData-> DxgkDdiIsSupportedVidPn;
+    DriverObjectExtension->DxgkDdiRecommendFunctionalVidPn = DriverInitData-> DxgkDdiRecommendFunctionalVidPn;
+    DriverObjectExtension->DxgkDdiEnumVidPnCofuncModality = DriverInitData-> DxgkDdiEnumVidPnCofuncModality;
+    DriverObjectExtension->DxgkDdiSetVidPnSourceAddress = DriverInitData-> DxgkDdiSetVidPnSourceAddress;
+    DriverObjectExtension->DxgkDdiSetVidPnSourceVisibility = DriverInitData-> DxgkDdiSetVidPnSourceVisibility;
+    DriverObjectExtension->DxgkDdiCommitVidPn = DriverInitData-> DxgkDdiCommitVidPn;
+    DriverObjectExtension->DxgkDdiUpdateActiveVidPnPresentPath = DriverInitData-> DxgkDdiUpdateActiveVidPnPresentPath;
+    DriverObjectExtension->DxgkDdiRecommendMonitorModes = DriverInitData-> DxgkDdiRecommendMonitorModes;
+    DriverObjectExtension->DxgkDdiRecommendVidPnTopology = DriverInitData-> DxgkDdiRecommendVidPnTopology;
+    DriverObjectExtension->DxgkDdiGetScanLine = DriverInitData-> DxgkDdiGetScanLine;
+    DriverObjectExtension->DxgkDdiStopCapture = DriverInitData-> DxgkDdiStopCapture;
+    DriverObjectExtension->DxgkDdiControlInterrupt = DriverInitData-> DxgkDdiControlInterrupt;
+    DriverObjectExtension->DxgkDdiCreateOverlay = DriverInitData-> DxgkDdiCreateOverlay;
+    DriverObjectExtension->DxgkDdiDestroyDevice = DriverInitData-> DxgkDdiDestroyDevice;
+    DriverObjectExtension->DxgkDdiOpenAllocation = DriverInitData-> DxgkDdiOpenAllocation;
+    DriverObjectExtension->DxgkDdiCloseAllocation = DriverInitData-> DxgkDdiCloseAllocation;
+    DriverObjectExtension->DxgkDdiRender = DriverInitData-> DxgkDdiRender;
+    DriverObjectExtension->DxgkDdiPresent = DriverInitData-> DxgkDdiPresent;
+    DriverObjectExtension->DxgkDdiUpdateOverlay = DriverInitData-> DxgkDdiUpdateOverlay;
+    DriverObjectExtension->DxgkDdiFlipOverlay = DriverInitData-> DxgkDdiFlipOverlay;
+    DriverObjectExtension->DxgkDdiDestroyOverlay = DriverInitData-> DxgkDdiDestroyOverlay;
+    DriverObjectExtension->DxgkDdiCreateContext = DriverInitData-> DxgkDdiCreateContext;
+    DriverObjectExtension->DxgkDdiDestroyContext = DriverInitData-> DxgkDdiDestroyContext;
+    DriverObjectExtension->DxgkDdiLinkDevice = DriverInitData-> DxgkDdiLinkDevice;
+    DriverObjectExtension->DxgkDdiSetDisplayPrivateDriverFormat = DriverInitData-> DxgkDdiSetDisplayPrivateDriverFormat;
+
+    /* Fill out the dispatch routines */;
     DriverExtend = DriverObject->DriverExtension;
     DriverObject->MajorFunction[IRP_MJ_CREATE] = (PDRIVER_DISPATCH)RDDM_MiniportDispatchCreate;
     DriverObject->MajorFunction[IRP_MJ_PNP] = (PDRIVER_DISPATCH)RDDM_MiniportDispatchPnp;
@@ -37,10 +141,17 @@ NTSTATUS
 NTAPI   //TODO: Implement me
 DxgkInternalDeviceIoctl(DEVICE_OBJECT *DeviceObject, IRP *Irp)
 {
+    PVOID *OutputBuffer;
     IO_STACK_LOCATION *IrpStack = Irp->Tail.Overlay.CurrentStackLocation;
     ULONG IoControlCode = IrpStack->Parameters.Read.ByteOffset.LowPart;
     switch (IoControlCode)
     {
+        case IOCTL_VIDEO_DDI_FUNC_REGISTER:
+            OutputBuffer = Irp->UserBuffer;
+            Irp->IoStatus.Information = 0;
+            Irp->IoStatus.Status = STATUS_SUCCESS;
+            *OutputBuffer = DpiInitialize;
+            DPRINT1("IOCTL_VIDEO_DDI_FUNC_REGISTER - Queued DpiInitialize up\n");
         default:
             DPRINT1("unknown IOCTRL Code: %X\n", IoControlCode);
             break;
