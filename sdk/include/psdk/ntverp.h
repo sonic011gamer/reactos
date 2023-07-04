8e1d989020338f73582691ae7b1cba3e2751a020
Justin Miller <Justinmiller100@gmail.com>
2023-07-03 22:41:37 -0700

    for now target vista

--- sdk/include/psdk/ntverp.h
+++ sdk/include/psdk/ntverp.h
@@ -13,9 +13,9 @@
  * PROGRAMMERS:     Alex Ionescu (alex.ionescu@reactos.org)
  */
 
-#define  TARGET_VERSION _WIN32_WINNT_WIN7
+#define  TARGET_VERSION _WIN32_WINNT_VISTA
 
-#if (TARGET_VERSION >= _WIN32_WINNT_WIN7)
+#if 0
 
 //
 // Windows NT 6.1.7601.17514
@@ -26,9 +26,9 @@
 #define VER_PRODUCTMINORVERSION             1
 #define VER_PRODUCTVERSION_W                (0x0601)
 #define VER_PRODUCTVERSION_DW               (0x06010000 | VER_PRODUCTBUILD)
+#endif
 
-#elif (TARGET_VERSION >= _WIN32_WINNT_VISTA)
-
+#if 1
 //
 // Windows NT 6.0.6002.18005
 //
@@ -38,9 +38,9 @@
 #define VER_PRODUCTMINORVERSION             0
 #define VER_PRODUCTVERSION_W                (0x0600)
 #define VER_PRODUCTVERSION_DW               (0x06000000 | VER_PRODUCTBUILD)
+#endif
 
-
-#else
+#if 0
 
 //
 // Windows NT Build 3790.3959
