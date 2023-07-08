1ffbca78c90950587eef3dec4ae6c269499b917b
Justin Miller <justin.miller@reactos.org>
2023-07-07 22:28:12 -0700

    [DXGKRNL] Create Initial driver - Support Vista style startup

---
+++ win32ss_vista/dxgkrnl/include/dxgkrnl.h
@@ -0,0 +1,19 @@
+/*
+ * PROJECT:     ReactOS Display Driver Model
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Core Header
+ * COPYRIGHT:   Copyright 2023 Justin Miller <justin.miller@reactos.org>
+ */
+
+#pragma once
+
+#include <ntddk.h>
+#include <windef.h>
+#include <ntstatus.h>
+
+#include <stdio.h>
+#include <locd3d.h>
+
+/* Create an IO request to fill out the function pointer list */
+#define IOCTL_VIDEO_DDI_FUNC_REGISTER \
+	CTL_CODE( FILE_DEVICE_VIDEO, 0xF, METHOD_NEITHER, FILE_ANY_ACCESS  )
