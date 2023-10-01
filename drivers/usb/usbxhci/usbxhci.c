566e1c936fb6b6ed185c8949f88a077b7961cec9
Justin Miller <justin.miller@reactos.org>
2023-10-01 00:33:53 -0700

    [USBXHCI] Properly Stub out XHCI_OPENENDPOINT

--- drivers/usb/usbxhci/usbxhci.c
+++ drivers/usb/usbxhci/usbxhci.c
@@ -395,8 +395,35 @@ XHCI_OpenEndpoint(
     _In_ PUSBPORT_ENDPOINT_PROPERTIES EndpointProps,
     _In_ PVOID Endpoint)
 {
-    DPRINT("XHCI_OpenEndpoint: UNIMPLEMENTED. FIXME\n");
-    return MP_STATUS_FAILURE;
+    PUSBPORT_ENDPOINT_PROPERTIES EndpointProperties = EndpointProps;
+    ULONG TransferType;
+
+    DPRINT("XHCI_OpenEndpoint: function initiated\n");
+    TransferType = EndpointProperties->TransferType;
+
+    switch (TransferType)
+    {
+        case USBPORT_TRANSFER_TYPE_ISOCHRONOUS:
+            return MP_STATUS_FAILURE;
+            break;
+
+        case USBPORT_TRANSFER_TYPE_CONTROL:
+            return MP_STATUS_FAILURE;
+            break;
+
+        case USBPORT_TRANSFER_TYPE_BULK:
+            return MP_STATUS_FAILURE;
+            break;
+
+        case USBPORT_TRANSFER_TYPE_INTERRUPT:
+            return MP_STATUS_FAILURE;
+            break;
+        default:
+            return MP_STATUS_NOT_SUPPORTED;
+            break;
+    }
+
+    return MP_STATUS_SUCCESS;
 }
 
 VOID
